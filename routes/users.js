6818812888e0c3b85f6d2e8c23c124bceac58458
Rokas <r.diedonis@yahoo.com>
2019-09-24 18:41:33 +0300

    quill implementation

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
-router.get("/login", (req, res) => res.render("login"));
-router.get("/register", (req, res) => res.render("register"));
+router.get('/login', (req, res) => res.render('login'))
+router.get('/register', (req, res) => res.render('register'))
 
 module.exports = router;
