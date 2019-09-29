50fabb06adfb107c6d1d4eae1b22e061e32f4597
Rokas <r.diedonis@yahoo.com>
2019-09-29 23:58:46 +0300

    added hapi joi validation to back end

--- routes/users.js
+++ routes/users.js
@@ -2,28 +2,77 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const db = require("../config/database")
+const jwt = require('jsonwebtoken');
+const Joi = require("@hapi/joi")
+
+// function which stores different error codes from validation
+// then transforms them to messages which can be sent to front end
+const validationErrorHandling = (error) => {
+    const errorCode = error[0].code
+    console.log(errorCode)
+    switch (errorCode) {
+        case "string.empty":
+            return new Error("Please provide username")
+            break
+        case "string.min":
+            return new Error(`Username should be at least ${error[0].local.limit} characters`)
+            break
+        case "string.max":
+            return new Error(`Username should be less than ${error[0].local.limit} characters`)
+            break
+        case "any.only":
+            return new Error("Passwords need to match")
+            break
+        default:
+            break
+    }
+}
+
+const passwordCheckRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+// Login validation schema
+const schema = Joi.object({
+    username: Joi.string().min(3).max(40).required().error((error) => {
+        return validationErrorHandling(error)
+    }),
+    email: Joi.string().min(6).required().email().error((error) => {
+        return validationErrorHandling(error)
+    }),
+    password1: Joi.string().pattern(passwordCheckRegex).error((error) => {
+        return validationErrorHandling(error)
+    }),
+    password2: Joi.valid(Joi.ref("password1")).error((error) => {
+        return validationErrorHandling(error)
+    })
+})
 
 // Example of db request
-// db.any('SELECT * FROM "Users"').then((data) => {
+// db.any('SELECT * FROM users').then((data) => {
 //     console.log(data)
 // }).catch((err) => { console.log(err) })
-const passwordCheckRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
 
 const createUserMiddleware = async (req, res, next) => {
     try {
         const saltRounds = 10;
 
         const hashPassword = await bcrypt.hash(req.body.password1, saltRounds)
-        console.log(hashPassword)
+        const newUsername = req.body.username
+        const newUserEmail = req.body.email
+
+        newUser = await db.one("INSERT INTO users(username, password, email) VALUES ($1, $2, $3) RETURNING id", [newUsername, hashPassword, newUserEmail])
+        console.log(newUser)
+        next()
+
     }
     catch (error) {
         console.error(error)
         next(error)
     }
-
-
 }
 
+const userSingIn = async (req, res, next) => {
+
+}
 
 router.get("/posts", (req, res) => res.render("posts", { title: "Posts" }));
 
@@ -31,31 +80,26 @@ router.get("/login", (req, res) => res.render("login", { title: "Sign In" }));
 
 router.get("/register", (req, res) => res.render("register", { title: "Sign Up" }));
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
+    const { error, value } = await schema.validate(req.body)
+    const errorMessage = error.message
 
     const { username, email, password1, password2 } = req.body;
 
-    let errors = [];
-
-    // Check required fields
-
-    if (!username || !email || !password1 || !password2) {
-        errors.push({ msg: "Please fill in all fields" });
+    if (errorMessage) {
+        return res.render("register", { errorMessage, username, email, password1, password2, title: "Sign Up" })
+    } else {
+        next()
     }
-    // Check if passwords match
-    if (password1 !== password2) {
-        errors.push({ msg: "Passwords don't match" });
-    }
-    // check password correctness
-    if (!passwordCheckRegex.test(password1)) {
-        errors.push({ msg: "Password minimum length eight characters, at least one uppercase letter, one lowercase letter and one number" });
-    }
-
-    //  check how many errors
-    if (errors.length > 0) {
-        return res.render("register", { errors, username, email, password1, password2, title: "Sign Up" })
-    } else { next() }
-
-}, createUserMiddleware)
+},
+    // createUserMiddleware,
+    //  userSingIn
+)
 
 module.exports = router;
+
+// username: Joi.string().required().error(new Error("Username is required")).min(3).error((err) => {
+//     return new Error(`Should be at least ${err[0].local.limit} characters long`)
+// }).max(40).error((err) => {
+//     return new Error(`Should be less than ${err[0].local.limit} characters`)
+// })
