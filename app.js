51b2303c4854a1a8223b4d8a3c0d1f73a955162c
Rokas <r.diedonis@yahoo.com>
2019-11-21 20:44:11 +0200

    Added Gulp task runner
    -Minifies, prefixes CSS
    -Started to work with BEM css
    -Made css for navbar

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 // Initiate request limiter
 app.use(limiter);
 app.use(
---
+++ gulpfile.js/index.js
@@ -0,0 +1,75 @@
+var sass = require("gulp-sass");
+const autoprefixer = require("autoprefixer");
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+var nodemon = require("gulp-nodemon");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+
+function css(cb) {
+  gulp
+    .src("./src/scss/project.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    // .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+  cb();
+}
+
+gulp.task("nodemon", cb => {
+  let started = false;
+  return nodemon({
+    script: "./bin/www"
+  }).on("start", () => {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
+gulp.task(
+  "browser-sync",
+  gulp.series("nodemon", () => {
+    browserSync.init(null, {
+      proxy: "http://localhost:3000",
+      files: ["src/**/*.*"],
+
+      port: 7000
+    });
+  })
+);
+
+function watch() {
+  gulp.watch("./src/scss/**/*", css);
+  // We should tell gulp which files to watch to trigger the reload
+  // This can be html or whatever you're using to develop your website
+  // Note -- you can obviously add the path to the Paths object
+  gulp.watch("views/**/*.ejs").on("change", browserSync.reload);
+}
+
+// Don't forget to expose the task!
+exports.watch = watch;
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp css
+exports.css = css;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.parallel(css, watch, "browser-sync");
+
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+gulp.task("build", build);
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task("default", build);
